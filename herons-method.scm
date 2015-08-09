39a92d26d26c682cba7d0482277cd4d69feba3bf
Thomas Varney <thomasvarney723@gmail.com>
2015-08-09 01:01:43 -0500

    initial commit

---
+++ herons-method.scm
@@ -0,0 +1,21 @@
+(define (average2 x y) 
+    (/ (+ x y) 2))
+
+(define (abs x) 
+    (if (< x 0)
+        (- x)
+        x))
+
+(define (difference x y) 
+    (abs (- x y)))
+
+(define (improve-guess guess number) 
+    (average2 guess (/ number guess)))
+
+(define (keep-improving guess number tolerance)  
+    (if (> tolerance (difference guess (improve-guess guess number)))
+        guess
+        (keep-improving (improve-guess guess number) number tolerance)))
+
+(define (sqrt number guess tolerance)    
+    (keep-improving guess number tolerance))
